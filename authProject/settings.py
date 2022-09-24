a549b1d8f262722bfffbd3e3e8f9778b044a2968
Mauricio Franco <mauriciofranco95@gmail.com>
2022-09-24 16:50:28 -0500

    deployment changes

--- authProject/settings.py
+++ authProject/settings.py
@@ -27,6 +27,7 @@ SECRET_KEY = 'django-insecure-w&f)+-%&$##j(2oys9=d%e-2^6&vc$_eh2k#y#m%1$o7w86ng0
 DEBUG = True
 
 ALLOWED_HOSTS = []
+CORS_ALLOW_ALL_ORIGINS = True
 
 
 # Application definition
@@ -40,6 +41,7 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'rest_framework',
     'authApp',
+    'corsheaders',
 ]
 
 SIMPLE_JWT = {
@@ -62,6 +64,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    "corsheaders.middleware.CorsMiddleware",
 ]
 
 REST_FRAMEWORK = {
